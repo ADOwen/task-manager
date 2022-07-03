8101dbc19fe0a66e339c6097ccd25f133375b3e8
ADOwen <92840021+ADOwen@users.noreply.github.com>
2022-07-03 17:08:09 -0400

    refactored and reorganized javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,32 +5,26 @@ const buttonEl = document.querySelector('#save-task')
 const tasksToDoEl = document.querySelector('#tasks-to-do')
 
 
-const addTaskHandler = function (event){  
+const taskFormHandler = function (event){  
   event.preventDefault()
   let taskNameInput = document.querySelector("input[name='task-name']").value
   let taskTypeInput = document.querySelector("select[name='task-type']").value
-  console.log(taskTypeInput)
   
-  const listItemEl = createTaskListEl()
-  const taskInfoEl = createTaskDivEl(taskNameInput, taskTypeInput)
- 
+  let taskDataObj = { name: taskNameInput, type: taskTypeInput }
+  
+  createTaskEl(taskDataObj)
+}
+
+const createTaskEl = function (taskDataObj) {
+  const listItemEl = document.createElement("li")
+  listItemEl.className = "task-item";
+  
+  const taskInfoEl = document.createElement("div")
+  taskInfoEl.className = "task-info"
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
   listItemEl.appendChild(taskInfoEl);
   tasksToDoEl.appendChild(listItemEl)
 }
 
-const createTaskListEl = function (){
-  const listItemEl = document.createElement("li")
-  listItemEl.className = "task-item";
- 
-  return listItemEl
-}
-
-const createTaskDivEl = function (name, type) {
-  const taskInfoEl = document.createElement("div")
-  taskInfoEl.className = "task-info"
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + name + "</h3><span class='task-type'>" + type + "</span>";
-
-  return taskInfoEl
-}
-
-formEl.addEventListener('submit', addTaskHandler)
+formEl.addEventListener('submit', taskFormHandler)
