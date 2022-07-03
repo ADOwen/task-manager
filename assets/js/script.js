9d506d0109bc5eae65348b8913962665bf14a0f2
ADOwen <92840021+ADOwen@users.noreply.github.com>
2022-07-03 16:49:39 -0400

    added the ability to display task types dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,33 @@ const buttonEl = document.querySelector('#save-task')
 
 const tasksToDoEl = document.querySelector('#tasks-to-do')
 
-const addTaskHandler = function (event){
-  
+
+const addTaskHandler = function (event){  
   event.preventDefault()
-
-  const taskItemEl = document.createElement("li")   
-  taskItemEl.className = 'task-item' 
-  taskItemEl.textContent = "This is a new task." 
-  tasksToDoEl.appendChild(taskItemEl)
-
+  let taskNameInput = document.querySelector("input[name='task-name']").value
+  let taskTypeInput = document.querySelector("select[name='task-type']").value
+  console.log(taskTypeInput)
+  
+  const listItemEl = createTaskListEl()
+  const taskInfoEl = createTaskDivEl(taskNameInput, taskTypeInput)
+ 
+  listItemEl.appendChild(taskInfoEl);
+  tasksToDoEl.appendChild(listItemEl)
 }
 
+const createTaskListEl = function (){
+  const listItemEl = document.createElement("li")
+  listItemEl.className = "task-item";
+ 
+  return listItemEl
+}
 
-formEl.addEventListener('click', addTaskHandler)
+const createTaskDivEl = function (name, type) {
+  const taskInfoEl = document.createElement("div")
+  taskInfoEl.className = "task-info"
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + name + "</h3><span class='task-type'>" + type + "</span>";
+
+  return taskInfoEl
+}
+
+formEl.addEventListener('submit', addTaskHandler)
